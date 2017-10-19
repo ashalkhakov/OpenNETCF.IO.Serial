ffd33e27971f630959849454d8bac0d8f14d043e
Artyom Shalkhakov <artyom@abitech.kz>
2017-10-19 17:52:03 +0600

    Separate builds per platform.

---
+++ build-android.cmd
@@ -0,0 +1,2 @@
+rem Android build
+"C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" "OpenNETCF.IO.Serial/OpenNETCF.IO.Serial.Android.csproj" /t:Build /p:Configuration=Release
---
+++ build-netcf.cmd
@@ -0,0 +1,2 @@
+rem .NET CF
+"C:\WINDOWS\Microsoft.NET\Framework\v3.5\MSBuild.exe" "OpenNETCF.IO.Serial/OpenNETCF.IO.Serial.csproj" /t:Build /p:Configuration=Release
